c91266e871d5911e97e31a9c50e8d03b256eb42c
DESKTOP-6BJNOTC\Home Dev <jota.dev@outlook.com>
2024-05-04 11:20:26 -0300

    Primer commit de configuracion

---
+++ src/main/java/com/api/gesproservice/GesproServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.gesproservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesproServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesproServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gesproservice/GesproServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gesproservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesproServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
